43de42a126d03f92eecb3dc913eb7828a95aeea6
Carlos Scheidegger <cscheid@cs.arizona.edu>
2015-11-12 15:00:55 -0700

    now with web workers and a real heatmap

---
+++ web/worker_compute_heatmap.js
@@ -0,0 +1,59 @@
+importScripts('lib/d3.js');
+
+var xArray;
+var yArray;
+
+dispatch = {
+    setYArray: function(e) {
+        yArray = new Float32Array(e.data.y.length);
+        xArray = new Float32Array(e.data.y.length);
+        for (var i=0; i<yArray.length; ++i) {
+            xArray[i] = Math.sqrt(e.data.y[i]);
+        }
+    },
+    compute: function(e) {
+        var data = e.data.data;
+        var min = 1e100, max = -1e100, lowDFrob = 0;
+        for (var i=0; i<data.length; ++i) {
+            var di = data[i];
+            var x1 = di[0], y1 = di[1];
+            for (var j=0; j<data.length; ++j) {
+                var dj = data[j];
+                var x2 = dj[0], y2 = dj[1];
+                var dx = x2 - x1, dy = y2 - y1;
+                var d2 = dx * dx + dy * dy, d = Math.sqrt(d2);
+                lowDFrob += d2;
+                yArray[i*data.length+j] = d;
+                min = Math.min(min, d);
+                max = Math.max(max, d);
+            }
+        }
+        debugger;
+        var xScale = d3.scale.linear().domain(e.data.xDomain).range(e.data.xRange);
+        var yScale = d3.scale.linear().domain([min, max]).range([e.data.yRange[1]-0.01, e.data.yRange[0]]).clamp(true); // uuugly
+        var dx = e.data.xRange[1] - e.data.xRange[0],
+            dy = e.data.yRange[1] - e.data.yRange[0];
+        var output = new Float32Array(dx * dy);
+        max = 0;
+        for (i=0; i<xArray.length; ++i) {
+            var xp = ~~xScale(xArray[i]), yp = ~~yScale(yArray[i]);
+            max = Math.max(max, ++output[xp + yp * dx]);
+        }
+        return {
+            max: max, 
+            dx: dx, dy: dy, frob: lowDFrob, 
+            yDomain: yScale.domain(), 
+            data: output
+        };
+    }
+};
+
+onmessage = function(e) {
+    var result = dispatch[e.data.call](e);
+    if (result !== undefined) {
+        if (e.data._uuid) {
+            result._uuid_dispatch = e.data._uuid;
+        }
+        postMessage(result);
+    }
+};
